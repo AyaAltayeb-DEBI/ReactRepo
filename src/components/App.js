efc09c84f29aa4c69a4e807ffadff5834db8e1e8
ADataScientist <ayaaltayeb.queensu@gmail.com>
2023-09-28 17:54:24 +0300

    update

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:"blue"}}>Hello React</h1>
+    <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+
+import "../styles/Nav.css"
+
+
+const Nav = ()=>{
+    return(
+        <div>
+        Hello from the nav :)
+        </div>
+    )
+}
+
+
+export default Nav
